953854628d00821a38c107cc7293ced068414cb8
Tarles95 <148482049+Tarles95@users.noreply.github.com>
2024-02-08 16:57:23 +0100

    Esercizio di oggi

---
+++ SQLQuery5.sql
@@ -0,0 +1,6 @@
+-- SELECT * FROM Impiegato	WHERE età > 29
+-- SELECT * FROM Impiegato WHERE RedditoMensile > 800
+-- SELECT * FROM Impiegato WHERE DetrazioneFiscale = 1
+-- SELECT * FROM Impiegato WHERE DetrazioneFiscale = 0
+-- SELECT * FROM Impiegato WHERE cognome like 'G%' order by cognome asc
+-- SELECT sum(RedditoMensile) as Totalereddito FROM Impiegato 
